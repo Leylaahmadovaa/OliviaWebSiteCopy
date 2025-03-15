c47d791554bd15b114955260374056dbd5d424c0
Leyla Ahmadova <leylaahmadova24@gmail.com>
2025-03-15 13:35:36 +0400

    header component is created

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLessThan, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faLessThan, faCartShopping, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faUser } from '@fortawesome/free-regular-svg-icons';
 export default function Header(){
-    return <div className="bg-[#F8F6F9] w-full h-[44px] flex justify-between px-[80px] text-[20px] items-center">
-        <ul className="flex">
+    return <>
+    {/* 1st */}
+    <div className="bg-[#F8F6F9] w-full h-[44px] flex justify-between px-[80px] text-[18px] items-center">
+        <ul className="flex gap-[25px]">
             <li>Mağazalar</li>
             <li>Korporativ Satışlar</li>
         </ul>
@@ -11,4 +14,41 @@ export default function Header(){
             <li className='flex items-center gap-[5px]'>AZ <FontAwesomeIcon icon={faLessThan} className='rotate-[-90deg] w-[8px]'/></li>
         </ul>
     </div>
+    {/* 2nd */}
+    <div className='flex justify-between px-[80px] py-[15px] items-center'>
+        <img src="https://olivia.az/media/logo/stores/2/Logo_desktop.png" alt="Olivia Logo" className='h-[30px]'/>
+        <div className='w-[830px] border-[1px] border-black  flex items-center px-[20px]'>
+            <FontAwesomeIcon icon={faMagnifyingGlass} className='w-[20px]'/>
+            <input placeholder='Axtarış...' type="text" className='border-none outline-none h-[41px] px-[10px]'/>
+        </div>
+        <ul className='text-[14px] flex items-center gap-[30px]'>
+            <li className='flex gap-[10px]'>
+            <FontAwesomeIcon icon={faHeart} className='w-[20px]'/>
+            Seçilmişlər
+            </li>
+            <li className='flex gap-[10px]'>
+            <FontAwesomeIcon icon={faCartShopping} className='w-[20px]'/>
+            Səbət
+            </li>
+        </ul>
+    </div>
+    {/* 3rd */}
+    <div className='border-t-2 border-b-2 h-[50px] w-full px-[50px] flex items-center'>
+        <ul className='flex gap-[22px] text-[15.5px]'>
+            <li className='cursor-pointer'>Kampaniya</li>
+            <li className='cursor-pointer'>Brands</li>
+            <li className='cursor-pointer'>Makiyaj</li>
+            <li className='cursor-pointer'>Üzə qulluq</li>
+            <li className='cursor-pointer'>Bədənə qulluq və vanna</li>
+            <li className='cursor-pointer'>Saçlara qulluq</li>
+            <li className='cursor-pointer'>Sağlamlıq və baxım</li>
+            <li className='cursor-pointer'>Ətriyyat</li>
+            <li className='cursor-pointer'>Uşaqlar üçün</li>
+            <li className='cursor-pointer'>Kişilər üçün</li>
+            <li className='cursor-pointer'>Ev və bağ üçün</li>
+            <li className='cursor-pointer'>Geyim və aksesuarlar</li>
+            <li className='cursor-pointer'>Dəstlər</li>
+        </ul>
+    </div>
+    </>
 }
--- utils/fontawesome.js
+++
@@ -1,4 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUser, faSearch, faLessThan } from '@fortawesome/free-solid-svg-icons';
-
-library.add( faUser, faSearch, faLessThan); // Add the icons you need
