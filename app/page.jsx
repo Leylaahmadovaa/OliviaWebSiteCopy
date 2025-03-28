4e3f0518e1bc253c9b46511e1c0fce1b46d7ef61
Leyla Ahmadova <leylaahmadova24@gmail.com>
2025-03-28 12:13:10 +0400

    olivia

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
 import { Carousel } from "@node_modules/antd/es";
-import Link from "@node_modules/next/link";
+import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../store/productSlice";
 import { categoryAtSaleLeadersActions } from "../store/categoryAtSaleLeadersSlice";
@@ -30,14 +30,18 @@ export default function Home() {
   };
 
   function handleChangeCategoryAtSatisLiderleri(category) {
-    dispatch(categoryAtSaleLeadersActions.changeCategoryAtSaleLeaders(category));
+    dispatch(
+      categoryAtSaleLeadersActions.changeCategoryAtSaleLeaders(category)
+    );
     localStorage.setItem("selectedCategory", category);
   }
 
   useEffect(() => {
     const savedCategory = localStorage.getItem("selectedCategory");
     if (savedCategory) {
-      dispatch(categoryAtSaleLeadersActions.changeCategoryAtSaleLeaders(savedCategory));
+      dispatch(
+        categoryAtSaleLeadersActions.changeCategoryAtSaleLeaders(savedCategory)
+      );
     }
   }, [dispatch]);
 
@@ -87,107 +91,153 @@ export default function Home() {
           {/* sub headers */}
           <ul className="flex gap-[30px] text-[18px] h-[40px]">
             <li
-            onClick={()=>handleChangeCategoryAtSatisLiderleri("Kirpiklər üçün tuş")}
-            className="cursor-pointer hover:border-b-[1px] pb-[10px] border-black hover:text-black hover:translate-y-[-1px]"
-            style={{
-              color:categoryAtSaleLeaders=="Kirpiklər üçün tuş"?"black":"gray",
-              fontWeight:categoryAtSaleLeaders=="Kirpiklər üçün tuş"?"600":"400"
-            }}
-            >Kirpiklər üçün tuş</li>
+              onClick={() =>
+                handleChangeCategoryAtSatisLiderleri("Kirpiklər üçün tuş")
+              }
+              className="cursor-pointer hover:border-b-[1px] pb-[10px] border-black hover:text-black hover:translate-y-[-1px]"
+              style={{
+                color:
+                  categoryAtSaleLeaders == "Kirpiklər üçün tuş"
+                    ? "black"
+                    : "gray",
+                fontWeight:
+                  categoryAtSaleLeaders == "Kirpiklər üçün tuş" ? "600" : "400",
+              }}
+            >
+              Kirpiklər üçün tuş
+            </li>
             <li
-            onClick={()=>handleChangeCategoryAtSatisLiderleri("Dodaqlar üçün")}
-            className="cursor-pointer hover:border-b-[1px] pb-[10px] border-black hover:text-black hover:translate-y-[-1px]"
-            style={{
-              color:categoryAtSaleLeaders=="Dodaqlar üçün"?"black":"gray",
-              fontWeight:categoryAtSaleLeaders=="Dodaqlar üçün"?"600":"400"
-            }}
-            >Dodaqlar üçün</li>
+              onClick={() =>
+                handleChangeCategoryAtSatisLiderleri("Dodaqlar üçün")
+              }
+              className="cursor-pointer hover:border-b-[1px] pb-[10px] border-black hover:text-black hover:translate-y-[-1px]"
+              style={{
+                color:
+                  categoryAtSaleLeaders == "Dodaqlar üçün" ? "black" : "gray",
+                fontWeight:
+                  categoryAtSaleLeaders == "Dodaqlar üçün" ? "600" : "400",
+              }}
+            >
+              Dodaqlar üçün
+            </li>
             <li
-            onClick={()=>handleChangeCategoryAtSatisLiderleri("Tonal kremlər")}
-            className="cursor-pointer hover:border-b-[1px] pb-[10px] border-black hover:text-black hover:translate-y-[-1px]"
-            style={{
-              color:categoryAtSaleLeaders=="Tonal kremlər"?"black":"gray",
-              fontWeight:categoryAtSaleLeaders=="Tonal kremlər"?"600":"400"
-            }}
-            >Tonal kremlər</li>
+              onClick={() =>
+                handleChangeCategoryAtSatisLiderleri("Tonal kremlər")
+              }
+              className="cursor-pointer hover:border-b-[1px] pb-[10px] border-black hover:text-black hover:translate-y-[-1px]"
+              style={{
+                color:
+                  categoryAtSaleLeaders == "Tonal kremlər" ? "black" : "gray",
+                fontWeight:
+                  categoryAtSaleLeaders == "Tonal kremlər" ? "600" : "400",
+              }}
+            >
+              Tonal kremlər
+            </li>
             <li
-            onClick={()=>handleChangeCategoryAtSatisLiderleri("Konsilerlər")}
-            className="cursor-pointer hover:border-b-[1px] pb-[10px] border-black hover:text-black hover:translate-y-[-1px]"
-            style={{
-              color:categoryAtSaleLeaders=="Konsilerlər"?"black":"gray",
-              fontWeight:categoryAtSaleLeaders=="Konsilerlər"?"600":"400"
-            }}
-            >Konsilerlər</li>
+              onClick={() =>
+                handleChangeCategoryAtSatisLiderleri("Konsilerlər")
+              }
+              className="cursor-pointer hover:border-b-[1px] pb-[10px] border-black hover:text-black hover:translate-y-[-1px]"
+              style={{
+                color:
+                  categoryAtSaleLeaders == "Konsilerlər" ? "black" : "gray",
+                fontWeight:
+                  categoryAtSaleLeaders == "Konsilerlər" ? "600" : "400",
+              }}
+            >
+              Konsilerlər
+            </li>
             <li
-            onClick={()=>handleChangeCategoryAtSatisLiderleri("Göz laynerləri")}
-            className="cursor-pointer hover:border-b-[1px] pb-[10px] border-black hover:text-black hover:translate-y-[-1px]"
-            style={{
-              color:categoryAtSaleLeaders=="Göz laynerləri"?"black":"gray",
-              fontWeight:categoryAtSaleLeaders=="Göz laynerləri"?"600":"400"
-            }}
-            >Göz laynerləri</li>
+              onClick={() =>
+                handleChangeCategoryAtSatisLiderleri("Göz laynerləri")
+              }
+              className="cursor-pointer hover:border-b-[1px] pb-[10px] border-black hover:text-black hover:translate-y-[-1px]"
+              style={{
+                color:
+                  categoryAtSaleLeaders == "Göz laynerləri" ? "black" : "gray",
+                fontWeight:
+                  categoryAtSaleLeaders == "Göz laynerləri" ? "600" : "400",
+              }}
+            >
+              Göz laynerləri
+            </li>
           </ul>
 
           {/* products */}
           <div className="h-[450px]">
-          {loading && <p>Loading...</p>}
-          {error && <p>Error: {error}</p>}
-          {products && products.length && (
-            <div className="mt-[30px]">
-            {products.some(product => product.category === categoryAtSaleLeaders) ? (
-              <Carousel
-                slidesToShow={6}
-                arrows={true}
-                slidesToScroll={1}
-                draggable={true}
-                className="w-[1400px]"
-              >
-                {products
-                  .filter(product => product.category === categoryAtSaleLeaders)
-                  .map((product) => (
-                    <div
-                      key={product.id}
-                      className="pr-[15px] w-[217px] h-[450px] relative bg-white border-r-[18px] border-[#eeeff1]"
-                    >
-                      <div>
-                        <FontAwesomeIcon
-                          className="absolute right-[15px] z-[1] top-[15px] text-gray-400 text-[25px]"
-                          width={20}
-                          icon={faHeart}
-                        />
-                        <img
-                          src={product.image}
-                          alt="product"
-                          className="translate-x-2 mt-[50px]"
-                        />
-                        <div className="bg-white h-[170px] flex flex-col justify-between pt-[50px] pl-[20px]">
-                          <div>
-                            <p className="text-gray-500 text-[12px]">{product.brand}</p>
-                            <p className="font-semibold">{product.title}</p>
-                          </div>
-                          <div className="flex justify-between items-center">
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error}</p>}
+            {products && products.length && (
+              <div className="mt-[30px]">
+                {products.some(
+                  (product) => product.category === categoryAtSaleLeaders
+                ) ? (
+                  <Carousel
+                    slidesToShow={6}
+                    arrows={true}
+                    slidesToScroll={1}
+                    draggable={true}
+                    className="w-[1400px]"
+                  >
+                    {products
+                      .filter(
+                        (product) => product.category === categoryAtSaleLeaders
+                      )
+                      .map((product) => (
+                        <div
+                          key={product.id}
+                          className="pr-[15px] w-[217px] h-[450px] relative bg-white border-r-[18px] border-[#eeeff1]"
+                        >
+                          <Link href={`/products/${product.id}`}>
                             <div>
-                              <p className="text-gray-500 text-[11px]">{product.weight}</p>
-                              <p className="font-bold mt-[2px] mb-[20px]">
-                                {product.price} &#x20BC;
-                              </p>
+                              <FontAwesomeIcon
+                                className="absolute right-[15px] z-[1] top-[15px] text-gray-400 text-[25px]"
+                                width={20}
+                                icon={faHeart}
+                              />
+                              <img
+                                src={product.image}
+                                alt="product"
+                                className="translate-x-2 mt-[50px]"
+                              />
+                              <div className="bg-white h-[170px] flex flex-col justify-between pt-[50px] pl-[20px]">
+                                <div>
+                                  <p className="text-gray-500 text-[12px]">
+                                    {product.brand}
+                                  </p>
+                                  <p className="font-semibold">
+                                    {product.title}
+                                  </p>
+                                </div>
+                                <div className="flex justify-between items-center">
+                                  <div>
+                                    <p className="text-gray-500 text-[11px]">
+                                      {product.weight}
+                                    </p>
+                                    <p className="font-bold mt-[2px] mb-[20px]">
+                                      {product.price} &#x20BC;
+                                    </p>
+                                  </div>
+                                  <FontAwesomeIcon
+                                    width={20}
+                                    icon={faCartShopping}
+                                    className="text-[20px] text-gray-400"
+                                  />
+                                </div>
+                              </div>
                             </div>
-                            <FontAwesomeIcon
-                              width={20}
-                              icon={faCartShopping}
-                              className="text-[20px] text-gray-400"
-                            />
-                          </div>
+                          </Link>
                         </div>
-                      </div>
-                    </div>
-                  ))}
-              </Carousel>
-            ) : (
-              <p className="text-center text-gray-500 mt-5">Məhsul tapılmadı</p>
+                      ))}
+                  </Carousel>
+                ) : (
+                  <p className="text-center text-gray-500 mt-5">
+                    Məhsul tapılmadı
+                  </p>
+                )}
+              </div>
             )}
-          </div>
-          )}
           </div>
 
           {/* button */}
